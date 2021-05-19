d047c4d3a50fd582f85181e1f1216dae97868faa
VQCNvwrL <dmytrokurilov@gmail.com>
2021-05-19 21:43:59 +0300

    unit tests done

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -13,22 +13,114 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testAddition() {
+    public void testAdditionPositiveValues() {
         float expectedResult = 13;
         float actualResult = calculator.addition(5.0f, 8.0f);
-        Assert.assertEquals(expectedResult, actualResult);
-
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
     }
 
     @Test
-    public void subtraction() {
+    public void testAdditionNegativeAndPositiveValues() {
+        float expectedResult = 10;
+        float actualResult = calculator.addition(-5.0f, 15.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
     }
 
     @Test
-    public void multiply() {
+    public void testAdditionNegativeValues() {
+        float expectedResult = -30;
+        float actualResult = calculator.addition(-20.0f, -10.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
     }
 
     @Test
-    public void dividing() {
+    public void testSubtractionPositiveValues() {
+        float expectedResult = 30;
+        float actualResult = calculator.subtraction(40.0f, 10.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testSubtractionPositiveAndNegativeValues() {
+        float expectedResult = 50;
+        float actualResult = calculator.subtraction(40.0f, -10.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testSubtractionNegativeValues() {
+        float expectedResult = -30;
+        float actualResult = calculator.subtraction(-40.0f, -10.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testMultiplyPositiveValues() {
+        float expectedResult = 40;
+        float actualResult = calculator.multiply(10.0f, 4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testMultiplyPositiveAndNegativeValues() {
+        float expectedResult = -40;
+        float actualResult = calculator.multiply(-10.0f, 4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testMultiplyNegativeValues() {
+        float expectedResult = 40;
+        float actualResult = calculator.multiply(-10.0f, -4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testDividingPositiveValues() {
+        float expectedResult = 40;
+        float actualResult = calculator.dividing(160.0f, 4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testDividingPositiveAndNegativeValues() {
+        float expectedResult = -40;
+        float actualResult = calculator.dividing(160.0f, -4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testDividingNegativeValues() {
+        float expectedResult = 40;
+        float actualResult = calculator.dividing(-160.0f, -4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testEquationSolutionAddition() {
+        float expectedResult = 40;
+        float actualResult = calculator.equationSolution(10.0f, "+", 30.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testEquationSolutionSubtraction() {
+        float expectedResult = 40;
+        float actualResult = calculator.equationSolution(50.0f, "-", 10.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testEquationSolutionMultiply() {
+        float expectedResult = 40;
+        float actualResult = calculator.equationSolution(10.0f, "*", 4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
+    }
+
+    @Test
+    public void testEquationSolutionDividing() {
+        float expectedResult = 40;
+        float actualResult = calculator.equationSolution(160.0f, "/", 4.0f);
+        Assert.assertEquals(expectedResult, actualResult, 0.000001);
     }
 }
